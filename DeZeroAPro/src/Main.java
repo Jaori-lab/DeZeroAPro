e74fbb00606e51abff928e36408bfaf39dbd96aa
Utilisateur <doaltoc@free.fr>
2022-06-18 13:19:06 +0200

    First Commit with Main

---
+++ DeZeroAPro/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Saloperie de git !!");
+		System.out.println("Je vais bien finir par y arriver , Bordel de merde !!");
+	}
+
+}
