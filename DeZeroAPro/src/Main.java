43ee1f260a57d253a9751bebde51d3e623722468
Utilisateur <doaltoc@free.fr>
2022-07-01 22:48:59 +0200

    Controlleur avec paramètres + Instance de Ville

--- DeZeroAPro/src/Main.java
+++ DeZeroAPro/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		// System.out.println("Je vais bien finir par y arriver , Bordel de merde !!\n");
 		// System.out.println("Ca y'est !! Enfin ..L'authentification par mot de passe \nest finie, il faut créer un token !");
 		
-		Ville v = new Ville();
+		Ville v = new Ville("Paris",15_000_000,"France"); 
 	}
 
 }
