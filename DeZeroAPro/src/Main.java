3509435e16375ee182a16ed6966be12e95d2c1d9
Utilisateur <doaltoc@free.fr>
2022-07-01 22:38:50 +0200

    Constructeur par défault et instance d'un premier objet de la Classe Ville

--- DeZeroAPro/src/Main.java
+++ DeZeroAPro/src/Main.java
@@ -1,9 +1,13 @@
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Saloperie de git !!");
-		System.out.println("Je vais bien finir par y arriver , Bordel de merde !!");
+		// System.out.println("Saloperie de git !!\n");
+		// System.out.println("Je vais bien finir par y arriver , Bordel de merde !!\n");
+		// System.out.println("Ca y'est !! Enfin ..L'authentification par mot de passe \nest finie, il faut créer un token !");
+		
+		Ville v = new Ville();
 	}
 
 }
